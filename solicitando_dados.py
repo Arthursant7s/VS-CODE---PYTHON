eef3c6c2051f07cbab73e062f864430cf1d67f6c
Arthursant7s <Arthuramaral232@gmail.com>
2026-02-11 17:01:25 -0300

    Add files via upload

---
+++ solicitando_dados.py
@@ -0,0 +1,17 @@
+import os 
+
+os.system("cls || clear")
+
+# ENTRADA
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura"))
+
+
+# SAIDA
+
+print("Nome:", nome)
+print("Idade: ", idade)
+print("Altura: ", altura)
+
