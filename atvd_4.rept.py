00bf8588cdd4d5069dbe68c3af38e4cd378a9918
Arthursant7s <Arthuramaral232@gmail.com>
2026-03-16 13:57:40 -0300

    Atualizar o atvd_4.rept.py

--- atvd_4.rept.py
+++ atvd_4.rept.py
@@ -1,4 +1,10 @@
 import os 
 import time
 
-os.system("cls")
+os.system("cls || clear")
+
+# Contagem regressiva de 10 a 1
+for i in range(10, 0, -1):
+    print(i)
+    # Esperar 1 segundo 
+    time.sleep(1)
