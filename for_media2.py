a0d57c3d4b2e7e4153fdacbcc1961145ac0d8580
Arthursant7s <Arthuramaral232@gmail.com>
2026-03-16 14:28:57 -0300

    Criar for_media2.py

---
+++ for_media2.py
@@ -0,0 +1,21 @@
+import os 
+
+os.system ("cls || clear") 
+
+soma_notas = 0 
+
+# Solicitar 3 notas 
+for i in range(1, 4):
+    nota = float(input(f"Digite a {i}° kota: "))
+    soma_notas += nota 
+  
+    # Calculando notas
+    media = soma_notas  / 3 
+    print(f"\nMédia final é: {media: .2f}")
+
+if media >= 7:
+    print("Você foi aprovado!") 
+
+else: 
+    print("Você foi reprovado.")
+    
