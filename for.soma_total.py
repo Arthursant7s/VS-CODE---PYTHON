bb8a743debd83ce82fef0bd0d965973872054948
Arthursant7s <Arthuramaral232@gmail.com>
2026-03-16 12:49:17 -0300

    Criar for.soma_total.py

---
+++ for.soma_total.py
@@ -0,0 +1,18 @@
+import os 
+
+os.system("cls || clear")
+
+# Inicializar a variável da soma com 0 
+soma = 0 
+
+print("Digite 5 numeros inteiros: ")
+
+# Laço para repetir 5 vezes 
+for i in range(1, 6):
+    numero = int(input(f"Digite o {i}° numero: "))
+    soma += numero 
+
+
+# Apresentar a soma final 
+print("-") 
+print(f"A soma dos 5 numeros é: {soma}") 
